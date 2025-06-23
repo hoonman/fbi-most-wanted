a6b37ae18aa06cb6a21e4ec6f032f42fd84e43d2
hoonman <jh020812@gmail.com>
2025-06-22 21:33:38 -0700

    edit data parser

--- python-scripts/data-parser.py
+++ python-scripts/data-parser.py
@@ -1,53 +1,43 @@
 import requests
 import json
-
-
-
-# 53 is the maximum number of pages. but this can probably change...
-
-# response = requests.get('https://api.fbi.gov/wanted/v1/list')
-# response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
-#     'page': 53
-# })
-
-# for i in range(0, 54):
-#     print(i)
-#     response = requests.get('https://api.fbi.gov/wanted/v1/list', params = {
-#         'page': i
-#     })
-#     data = json.loads(response.content)
-
-#     # load the data into a file
-#     with open(f"fbi-data-{i}.json", "w") as f:
-#         json.dump(data, f, indent=4)
-
-
-
-# some questions we can answer with this data
-# out of the fbi's most wanted list, how many have unknown name? unknown height? 
-
-
-
-
-# let us design this parser.
-# this parser will have the following methods
-
-# function that will load json data into a dictionary so we can easily use it
-# input: maximum page number, 
-# output: dicionary of fbi's most wanted criminals
+import time
 
 class Parser:
+    most_wanted_list = []
 
     def __init__(self):
-        self.total_count = 0
-        self.most_wanted = {}
+        self.actual_total = 0 # shared across all instances 
 
     def get_fbi_data(self, max_page_num):
-        # we are going to try except here i think
+        response = requests.get('https://api.fbi.gov/wanted/v1/list')
+        data = json.loads(response.content) 
 
-        for i in range(1, max_page_num):
-            response = requests.get('https://api.fbi.gov/wanted/v1/list', params={'page':i})
-            new_map = json.loads(response.content)
+        page_num = 1
+        current_total = 0
+        self.actual_total = data['total']
+        most_wanted_list = []
+
+        while current_total < self.actual_total:
+            response = requests.get('https://api.fbi.gov/wanted/v1/list', params={'page':page_num})
+            if response.status_code != 200 or not response.content:
+                print(f"Error or empty response at page {page_num}, stopping. status code was: {response.status_code}")
+                break
+            try:
+                data = json.loads(response.content)
+                if 'items' not in data or not data['items']:
+                    print(f"No items found at page {page_num}, stopping")
+                    break
+                most_wanted_list.extend(data['items'])
+                data_items_num = len(data['items'])
+                current_total += data_items_num
+                print("finished page num: ", page_num, "\t total: ", current_total)
+                page_num += 1
+                if page_num % 10 == 0:
+                    print("sleeping...")
+                    time.sleep(5)
+            except json.JSONDecodeError:
+                print(f"Invalid JSON response at page {page_num}, stopping")
+                break
 
 
 parser = Parser()
