6f29108d76c330ba6d505c21481bf3eaa5f252bf
hoonman <jh020812@gmail.com>
2025-06-28 16:43:50 -0700

    add boilerplate ui code

---
+++ react-app/src/components/FBIDashboard.tsx
@@ -0,0 +1,8 @@
+
+const FBIDashboard: React.FC = () => {
+    return (
+        <h1>Hello world</h1>
+    );
+}
+
+export default FBIDashboard;
---
+++ react-app/src/components/FBIService.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const fetchMostWanted = async (page=1, pageSize=20, filters={}) => {
+    const params = { page, page_size: pageSize, ...filters};
+    const res = await axios.get('/api/most_wanted', { params });
+    return res.data;
+}
+
+export const fetchDetails = async (id: unknown) => {
+    const res = await axios.get(`/api/most_wanted/${id}`)
+    return res.data
+}
+
+export const fetchStats = async () => {
+    const res = await axios.get('/api/stats')
+    return res.data
+}
+
